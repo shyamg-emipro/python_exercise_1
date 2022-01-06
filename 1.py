4e4e8ec852b8378c0efa80010eb1160aeabbdf9b
shyamg <shyamg@emiprotechnologies.com>
2022-01-06 11:34:47 +0530

    [1.py]

--- 1.py
+++ 1.py
@@ -1,1 +1,11 @@
-print("hello")
+# Write a Python program which accepts a sequence of comma-separated numbers from the user and generates a list and a tuple with those numbers.
+# Sample data : 3, 5, 7, 23
+# Output :
+# List : ['3', ' 5', ' 7', ' 23']
+# Tuple : ('3', ' 5', ' 7', ' 23')
+
+data = input("Enter comma separated numbers:  ")  # Input from user
+list1 = [int(x) for x in list(data.split(","))]  # split data and convert to list and change list items from str to int using list comprehension
+tuple1 = tuple(list1)
+print("List:  ", list1)
+print("Tuple: ", tuple1)
