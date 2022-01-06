4456005aaded3b23da866214e92ad3faa93905b5
shyamg <shyamg@emiprotechnologies.com>
2022-01-06 11:37:49 +0530

    [2.py initial commit]

---
+++ 2.py
@@ -0,0 +1,6 @@
+# Write a Python program to display the first and last colors from the following list.
+#   color_list = ["Red","Green","White" ,"Black"]
+
+color_list = ["Red", "Green", "White", "Black"]
+print("First Color from the List: ", color_list[0])
+print("Last Color from the List: ", color_list[len(color_list) - 1])
